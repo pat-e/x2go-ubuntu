210c7493f7d855b1a7338a4ace1542d84c5a7223
pat-e <paternst@gmail.com>
2017-01-05 11:05:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 
-# Check if the script is being run as root 
-
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
 # Update repositories and upgrade packages
 
-apt-get update
+sudo apt-get update
+
+sudo apt-get install -y software-properties-common
 
 # Add x2go ppa; update repo db
 
-add-apt-repository -y ppa:x2go/stable
+sudo add-apt-repository -y ppa:x2go/stable
 
-apt-get -y update
+sudo apt-get -y update
 
 # Install xfce4
 
-apt-get -y install xfce4
+sudo apt-get -y install xfce4
 
 # Install ubuntu software center
 
-apt-get -y install software-center
-apt-get -y install synaptic
+sudo apt-get -y install software-center
+sudo apt-get -y install synaptic
 
 # Install x2goserver
 
-apt-get -y install x2goserver*
+sudo apt-get -y install x2goserver*
 
 # Start x2goserver
 
-service x2goserver start
+sudo service x2goserver start
 
 echo "The server has now been setup. To connect to it, please use x2goclient, available from here : http://wiki.x2go.org/doku.php/doc:installation:x2goclient \n"
-echo "Please login using your root credentials when using x2goclient; choose xfce as the desktop environment"
